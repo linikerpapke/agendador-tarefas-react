18be3fe1e3a009b8ec123ba0a4a2d74771ed9e48
Liniker <papke.liniker@gmail.com>
2026-03-17 00:28:26 -0300

    create login in userService

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -6,11 +6,21 @@ export interface RegisterRequest {
     senha: string,
 }
 
+export interface LoginRequest {
+    email: string,
+    senha: string,
+}
+
 export const userService = {
 
     async register(data: RegisterRequest) {
         const response = await api.post("/usuario", data);
         return response.data;
+    },
+
+    async login(data: LoginRequest) {
+        const response = await api.post("/usuario/login", data)
+        return response.data
     }
 
 }
